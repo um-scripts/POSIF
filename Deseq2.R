27e4167a97b751a3fc0ec140395e39abc82d9fdc
um-scripts <149608405+um-scripts@users.noreply.github.com>
2025-02-06 12:16:47 +0530

    Update Deseq2.R

--- Deseq2.R
+++ Deseq2.R
@@ -2,41 +2,27 @@ library(DESeq2)
 library(tidyverse)
 
 counts_data <- read.csv('reads.csv', header = TRUE, row.names = 1)
-colnames(counts_data)
-head(counts_data)
-
 sample_info <- read.csv('info.csv', header = TRUE, row.names = 1)
-colnames(sample_info)
-head(sample_info)
 
-all(colnames(counts_data) %in% rownames(sample_info))
-all(colnames(counts_data) == row.names(sample_info))
+stopifnot(identical(colnames(counts_data), rownames(sample_info)))
 
+dds <- DESeqDataSetFromMatrix(countData = counts_data, 
+                              colData = sample_info, 
+                              design = ~ Stress)
 
-dds <-DESeqDataSetFromMatrix(countData = counts_data, 
-                             colData = sample_info, 
-                             design = ~ Stress)
-dds <- estimateSizeFactors(dds)
-#=================To save the normalized read counts===========================
-#dds <- estimateSizeFactors(dds)
-#sizeFactors(dds)
-#normalized_counts <- counts(dds, normalized= TRUE)
-#normalized_counts
-#write.table(normalized_counts, file="normalized_counts.tsv", sep= "\t", quote=F, col.names = NA)
-#============================================
-dds
-
-#SetFactorLevel
 dds$Stress <- relevel(dds$Stress, ref = "untreated")
 
-
 dds <- DESeq(dds)
-res <- results(dds)
-plotMA(res)
 
+res <- results(dds)
 res0.05 <- results(dds, alpha = 0.05)
+
+normalized_counts <- counts(dds, normalized= TRUE)
+write.table(normalized_counts, file="normalized_counts.tsv", sep= "\t", quote=F, col.names = NA)
+
+write.csv(as.data.frame(res0.05), "DE_results.csv")
+
+plotMA(res0.05)
+
 summary(res)
 summary(res0.05)
-
-
-
